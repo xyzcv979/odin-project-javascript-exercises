92d302fe14dc938961e0060337cfaf82b3c354e1
xyzcv979 <avon1462@gmail.com>
2024-03-10 21:23:36 -0500

    finished up to 07 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    let returnString = "";
+    for (let i = 0; i < num; i++) {
+        returnString += word;
+    }
+    return returnString;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+        return "ERROR";
+    }
+    let sum = 0;
+    let minNum = Math.min(a,b);
+    let maxNum = Math.max(a,b);
+    for (let i = minNum; i <= maxNum; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
